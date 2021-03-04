76a63d9ef85b4eac95045854d95462f0e37986d4
PDieE <23275429@qq.com>
2021-03-04 11:35:24 +0800

    1.nuxt升级到2.15.2 2.升级所有依赖到最新版本

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,6 +60,13 @@ export default {
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
   axios: {},
 
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en',
+    },
+  },
+
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {},
 }
